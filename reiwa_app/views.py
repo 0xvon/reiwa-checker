eebe90dc76bfefc483abf08a6e0265e43587410c
masatsch <masato.11.soccer@gmail.com>
2019-05-01 22:40:21 +0900

    optimize time sleep and clear cache

--- reiwa_app/views.py
+++ reiwa_app/views.py
@@ -12,6 +12,7 @@ import numpy as np
 
 
 def model_form_upload(request):
+    cache.clear()
     global input_path
     global form
     # shutil.rmtree('media/documents')
@@ -29,21 +30,27 @@ def model_form_upload(request):
             return redirect('calc')
     else:
         form = DocumentForm()
-        
     return render(request, 'reiwa_app/model_form_upload.html', {
         'form': form,
     })
 
 def calc(request):
+    while input_path is False:
+        time.sleep(0.1)
+
     reiwa_path = cv2.imread("reiwa_app/static/reiwa_app/assets/reiwa.jpg")
     image_path = cv2.imread(input_path)
+    while image_path is False:
+        time.sleep(0.1)
+
     reiwa_path = cv2.resize(reiwa_path, (32, 32))
     image_path = cv2.resize(image_path, (32, 32))
     reiwa_path = np.ravel(reiwa_path)
     image_path = np.ravel(image_path)
     global similarity
     similarity = round(1e7 * np.dot(image_path, reiwa_path) / (np.linalg.norm(image_path) * np.linalg.norm(reiwa_path)))
-    time.sleep(5)
+    while similarity is False:
+        time.sleep(0.1)
     return redirect('result')
     # return render(request, 'reiwa_app/model_form_upload.html', {
     #     'form': form,
@@ -54,8 +61,12 @@ def calc(request):
 def result(request):
     if request.method == 'POST':
         os.remove(input_path)
-        cache.clear()
         return redirect('upload')
+    
+    
+    while input_path is False or similarity is False:
+        time.sleep(0.1)
+        
     return render(request, 'reiwa_app/result.html', {
                 'data': input_path,
                 'similarity': similarity,
