6a4cecef6d578a558267e9738dd7ba4dd2695701
masatsch <masato.11.soccer@gmail.com>
2019-05-02 01:01:04 +0900

    accurate

--- reiwa_app/views.py
+++ reiwa_app/views.py
@@ -8,20 +8,15 @@ import numpy as np
 import cv2
 import os
 import time
-import numpy as np
 
 
 def model_form_upload(request):
     cache.clear()
     global input_path
     global form
-    # shutil.rmtree('media/documents')
-    # os.mkdir('media/documents')
+
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
-        # max_id = Document.objects.latest('id').id
-        # obj = Document.objects.get(id=0)
-        # input_path = settings.BASE_DIR + obj.photo.url
         input_path = settings.MEDIA_URL + "documents/" + str(request.FILES['photo'])
         if form.is_valid():
             form.save()
@@ -35,40 +30,37 @@ def model_form_upload(request):
     })
 
 def calc(request):
-    while input_path is False:
-        time.sleep(0.1)
-
-    reiwa_path = cv2.imread("reiwa_app/static/reiwa_app/assets/reiwa.jpg")
-    image_path = cv2.imread(input_path)
-    while image_path is False:
-        time.sleep(0.1)
-
-    reiwa_path = cv2.resize(reiwa_path, (32, 32))
-    image_path = cv2.resize(image_path, (32, 32))
+    time.sleep(2)
+    global reiwa_path
+    reiwa_path = cv2.imread("reiwa_app/static/reiwa_app/assets/reiwa.jpg", 0)
+    global image_path
+    image_path = cv2.imread(input_path, 0)
+    reiwa_path = cv2.resize(reiwa_path, (100, 100))
+    image_path = cv2.resize(image_path, (100, 100))
     reiwa_path = np.ravel(reiwa_path)
     image_path = np.ravel(image_path)
     global similarity
     similarity = round(1e7 * np.dot(image_path, reiwa_path) / (np.linalg.norm(image_path) * np.linalg.norm(reiwa_path)))
-    while similarity is False:
-        time.sleep(0.1)
-    return redirect('result')
-    # return render(request, 'reiwa_app/model_form_upload.html', {
-    #     'form': form,
-    #     'path': input_path
-    # })
+    eucrid_distance = np.linalg.norm(np.abs(reiwa_path - image_path), ord=2)
+    if eucrid_distance < 1:
+        similarity = 100.0
+    elif eucrid_distance < 10:
+        similarity = 80.0
+    elif eucrid_distance < 100:
+        similarity = 60.0
+    elif eucrid_distance < 300:
+        similarity = 50.0
     
+    time.sleep(2)
+    return redirect('result')
 
 def result(request):
     if request.method == 'POST':
         os.remove(input_path)
         return redirect('upload')
     
-    
-    while input_path is False or similarity is False:
-        time.sleep(0.1)
-        
     return render(request, 'reiwa_app/result.html', {
                 'data': input_path,
-                'similarity': similarity,
+                'similarity': similarity
             })
     
