ac2a95adb04858093a598b63a2b65af01ed8d14a
masatsch <masato.11.soccer@gmail.com>
2019-05-01 16:10:03 +0900

    security

--- reiwa_app/views.py
+++ reiwa_app/views.py
@@ -4,6 +4,8 @@ from .models import Document
 from reiwa_prj import settings
 import numpy as np
 import cv2
+import os
+import shutil
 import numpy as np
 from PIL import Image
 
@@ -11,28 +13,28 @@ from PIL import Image
 def model_form_upload(request):
     global input_path
     global form
+    shutil.rmtree('media/documents')
+    os.mkdir('media/documents')
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         # max_id = Document.objects.latest('id').id
         # obj = Document.objects.get(id=0)
         # input_path = settings.BASE_DIR + obj.photo.url
         input_path = settings.MEDIA_URL + "documents/" + str(request.FILES['photo'])
-
         if form.is_valid():
             form.save()
-            form = None
+            form = DocumentForm()
             return redirect('calc')
     else:
+        input_path = None
         form = DocumentForm()
         
     return render(request, 'reiwa_app/model_form_upload.html', {
-        'form': form
+        'form': form,
+        'path': input_path
     })
 
 def calc(request):
-    if request.method == 'POST':
-        return redirect('upload')
-
     reiwa_path = cv2.imread("reiwa_app/static/reiwa_app/assets/reiwa.jpg")
     image_path = cv2.imread(input_path)
     reiwa_path = cv2.resize(reiwa_path, (32, 32))
@@ -42,10 +44,15 @@ def calc(request):
     global similarity
     similarity = round(1e7 * np.dot(image_path, reiwa_path) / (np.linalg.norm(image_path) * np.linalg.norm(reiwa_path)))
     return redirect('result')
+    # return render(request, 'reiwa_app/model_form_upload.html', {
+    #     'form': form,
+    #     'path': input_path
+    # })
     
 
 def result(request):
     if request.method == 'POST':
+        # os.remove(input_path)
         return redirect('upload')
     return render(request, 'reiwa_app/result.html', {
                 'data': input_path,
