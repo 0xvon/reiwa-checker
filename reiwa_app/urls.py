2e321bfe801860b890cfa9d98deb53cdddf18fd7
masatsch <masato.11.soccer@gmail.com>
2019-05-04 18:37:04 +0900

    unify view method

--- reiwa_app/urls.py
+++ reiwa_app/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.model_form_upload, name='upload'),
-    path('calc', views.calc, name='calc'),
-    path('result', views.result, name='result'),
 ]
