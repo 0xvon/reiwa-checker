bc72fedcceab27bb6ee049b065371988865e1dff
masatsch <masato.11.soccer@gmail.com>
2019-05-16 15:00:49 +0900

    complete error process

--- reiwa_app/views.py
+++ reiwa_app/views.py
@@ -11,58 +11,43 @@ import time
 
 
 def model_form_upload(request):
-    if request.method == 'POST':
-        if request.FILES == None:
-            form = DocumentForm()
-            cache.clear()
-            return render(request, 'reiwa_app/model_form_upload.html', {
-                'form': form,
-                'error_message': 'ファイルを選択してください'
-            })
-        form = DocumentForm(request.POST, request.FILES)
-        input_path = settings.MEDIA_URL + "documents/" + str(request.FILES['photo'])
-        if form.is_valid():
-            form.save()
-            form = DocumentForm()
-            time.sleep(3)
-            reiwa_path = cv2.imread("reiwa_app/static/reiwa_app/assets/reiwa.jpg", 0)
-            image_path = cv2.imread(input_path, 0)
-            if image_path == None:
+    try:
+        if request.method == 'POST':
+            form = DocumentForm(request.POST, request.FILES)
+            input_path = settings.MEDIA_URL + "documents/" + str(request.FILES['photo'])
+            if form.is_valid():
+                form.save()
                 form = DocumentForm()
-                cache.clear()
-                return render(request, 'reiwa_app/model_form_upload.html', {
-                    'form': form,
-                    'error_message': 'ファイル名が正しくありません'
+                time.sleep(3)
+                reiwa_path = cv2.imread("reiwa_app/static/reiwa_app/assets/reiwa.jpg", 0)
+                image_path = cv2.imread(input_path, 0)
+                reiwa_path = cv2.resize(reiwa_path, (100, 100))
+                image_path = cv2.resize(image_path, (100, 100))
+                reiwa_path = np.ravel(reiwa_path)
+                image_path = np.ravel(image_path)
+                similarity = round(np.dot(1e7 * image_path, reiwa_path) / (np.linalg.norm(image_path) * np.linalg.norm(reiwa_path)))
+                eucrid_distance = np.linalg.norm(np.abs(reiwa_path - image_path), ord=2)
+                if eucrid_distance < 5000:
+                    similarity = 100
+                elif eucrid_distance < 10000:
+                    similarity = round(np.random.rand() * 80 + 19)
+                elif eucrid_distance < 13599:
+                    similarity = round(np.random.rand() * 60 + 20)
+                elif eucrid_distance < 20000:
+                    similarity = round(np.random.rand() * 60)
+                return render(request, 'reiwa_app/result.html', {
+                    'data': input_path,
+                    'similarity': similarity
+                    # 'eucrid_distance': eucrid_distance
                 })
-            reiwa_path = cv2.resize(reiwa_path, (100, 100))
-            image_path = cv2.resize(image_path, (100, 100))
-            reiwa_path = np.ravel(reiwa_path)
-            image_path = np.ravel(image_path)
-            similarity = round(np.dot(1e7 * image_path, reiwa_path) / (np.linalg.norm(image_path) * np.linalg.norm(reiwa_path)))
-            eucrid_distance = np.linalg.norm(np.abs(reiwa_path - image_path), ord=2)
-            if eucrid_distance < 5000:
-                similarity = 100
-            elif eucrid_distance < 10000:
-                similarity = round(np.random.rand() * 80 + 19)
-            elif eucrid_distance < 13599:
-                similarity = round(np.random.rand() * 60 + 20)
-            elif eucrid_distance < 20000:
-                similarity = round(np.random.rand() * 60)
-            return render(request, 'reiwa_app/result.html', {
-                'data': input_path,
-                'similarity': similarity
-                # 'eucrid_distance': eucrid_distance
-            })
         else:
             form = DocumentForm()
             cache.clear()
             return render(request, 'reiwa_app/model_form_upload.html', {
                 'form': form,
-                'error_message': '<p>エラーーーーーー</p>'
             })
-    else:
-        form = DocumentForm()
-        cache.clear()
+    except :
         return render(request, 'reiwa_app/model_form_upload.html', {
-            'form': form,
-        })
+                    'form': form,
+                    'error_message': 'ファイルを正しく選択してください'
+               })
